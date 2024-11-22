7a62aac799537fd5696f1b2d6f7f9b79cafdd084
discip <53649486+discip@users.noreply.github.com>
2024-11-22 07:19:52 +0100

    Update Tab.cpp

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -4591,7 +4591,6 @@ void TabPrinter::toggle_options()
         //toggle_option("retraction_distances_when_cut", m_config->opt_bool("long_retractions_when_cut",i),i);
         
         toggle_option("travel_slope", m_config->opt_enum("z_hop_types", i) != ZHopType::zhtNormal, i);
-	toggle_line("travel_slope", m_config->opt_enum("z_hop_types", i) != ZHopType::zhtNormal, i);
     }
 
     if (m_active_page->title() == L("Motion ability")) {
